7e61750ab21f1c925add8a01c0c412ecb419f65d
Arthur Pedroti <arthurpedroti@gmail.com>
2021-01-29 11:59:46 -0300

    refactor: addding group 0010 to opcosts and filter some part numbers

--- src/pages/OpCosts/index.tsx
+++ src/pages/OpCosts/index.tsx
@@ -15,7 +15,7 @@ export interface Data {
 
 const OpCosts: React.FC = () => {
   const { data } = useFetch<Data[]>(
-    'op-costs?filial=0101&ano=2019,%202020,%202021&grupo=0080,%200094,%200500,%200501,%200502,%200503,%200510,%200520,%200540,%200550,%200560,%200570',
+    'op-costs?filial=0101&ano=2019,%202020,%202021&grupo=0010,%200080,%200094,%200500,%200501,%200502,%200503,%200510,%200520,%200540,%200550,%200560,%200570',
   );
 
   if (!data) {
@@ -25,6 +25,21 @@ const OpCosts: React.FC = () => {
       </Container>
     );
   }
+  // console.log(data);
+  const arr = [
+    'BH05P01',
+    'BH05P02',
+    'BH05P03',
+    'BH05P04',
+    'BH05P05',
+    'BH05P06',
+    'BH05P07',
+    'BH05P08',
+  ];
+  const newData = data.filter(
+    (dataItem: Data) => !arr.includes(dataItem.PRODUTO),
+  );
+  console.log(newData);
 
   return (
     <Container>
@@ -37,7 +52,7 @@ const OpCosts: React.FC = () => {
         </HeaderMeta>
       </Header>
       <PivotTable
-        data={data}
+        data={newData}
         rows={['PRODUTO']}
         cols={['ANO', 'MES']}
         aggregatorName="Average"
