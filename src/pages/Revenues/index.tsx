90e4e5079326724715cdc64c8ae258d336d3f893
Arthur Pedroti <arthurpedroti@gmail.com>
2021-04-21 18:09:58 -0300

    feat: updating revenues power bi report

--- src/pages/Revenues/index.tsx
+++ src/pages/Revenues/index.tsx
@@ -13,7 +13,7 @@ const Revenues: React.FC = () => {
           title="Faturamento"
           width="1280"
           height="768"
-          src="https://app.powerbi.com/view?r=eyJrIjoiZWNiZDIyZWYtNWQzYS00Yjg0LTllZTEtMzc5NGNkNzUyOTFlIiwidCI6IjY3ZTY2NjE5LWMwMTAtNDVlZi05ZWJmLTY0OTI4OTUzNDE5YyJ9&pageName=ReportSection"
+          src="https://app.powerbi.com/view?r=eyJrIjoiZWNiZDIyZWYtNWQzYS00Yjg0LTllZTEtMzc5NGNkNzUyOTFlIiwidCI6IjY3ZTY2NjE5LWMwMTAtNDVlZi05ZWJmLTY0OTI4OTUzNDE5YyJ9&pageName=ReportSectioneee129ed98772374c430"
           frameBorder="0"
           allowFullScreen
         />
